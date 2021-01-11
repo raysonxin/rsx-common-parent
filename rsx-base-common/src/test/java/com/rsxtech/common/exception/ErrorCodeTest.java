2f021421dc48bf6ba26c8b5fd97c1b3c92eba08a
raysonxin <xindachao@gmail.com>
2021-01-11 10:19:41 +0800

    删除测试方法

---
+++ rsx-base-common/src/test/java/com/rsxtech/common/exception/ErrorCodeTest.java
@@ -0,0 +1,19 @@
+//package com.rsxtech.common.exception;
+//
+//import com.rsxtech.common.exception.enums.CommonCodeEnum;
+//import org.junit.Test;
+//
+///**
+// * 测试
+// *
+// * @author raysonxin
+// * @since 2021/1/9
+// */
+//public class ErrorCodeTest {
+//
+//    @Test
+//    public void testErrorCode() {
+//        CommonCodeEnum.OBJECT_NULL_ERROR.assertNotNull(null,"abcd");
+//    }
+//
+//}
